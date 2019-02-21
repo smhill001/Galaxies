6aa169d3a9eade22a1debb55cb109121145ffc73
Steven Hill <smhill001@gmail.com>
2019-02-21 12:05:42 -0700

    Major Rewrite
    Refactored and commented code. Consolidated classes and functions to use Utils libraries. Reduced code in GalaxyLIB.py by 50% or more.

--- GalaxyLIB.py
+++ GalaxyLIB.py
@@ -14,122 +14,46 @@ sys.path.append(drive+'\\Astronomy\Python Play\Util')
 
 import ConfigFiles as CF
 
-class Astrophysical_Parameters:
-    def __init__(self,drive,TargetID):
-        """
-        This is the BASE CLASS for astrophysical object parameters.  ALL objects
-        posess at least an identifier, object type and location.
-        
-        SMH 1/11/18
-        """
-    
-        self.TargetID=TargetID
-        self.TargetType=''  #PN, ST, etc.
-        self.RA2000=0.0 #in degrees
-        self.DE2000=0.0 #in degrees
-        self.Distance=0.0 
-        self.DistUnits='' #different for different scales: "AU", "LY" for now
-        self.mV=0.0 #Visual band magnitude
-
-
-class Galaxy_Parameters(Astrophysical_Parameters):
-    """
-    This class builds on the base class to add parameters specific to
-    galaxies: position angle, inclination, and classification. Note that
-    for other deep sky objects, in particular planetary nebulae, the 
-    parameters would be identical or mostly identical. 
-    
-    SMH 1/11/18
-    """
-    def __init__(self,drive,TargetID):
-        #View has two options: raw or flux?
-        #Test_plot_params_base.__init__(self,drive,ObjIdentifierDD)
-        self.PA=0.0 #degrees
-        self.Inclination=0.0 #degrees
-        self.classification='' #classification
-        CfgFile=open(drive+'/Astronomy/Python Play/Galaxies/Galaxy_Parameters.txt','r')
-        CfgLines=CfgFile.readlines()
-        CfgFile.close()
-        nrecords=len(CfgLines)
-        #print CfgLines
-
-        for recordindex in range(1,nrecords):
-            fields=CfgLines[recordindex].split(',')
-            #print fields[0], fields[1]
-            if fields[0] == TargetID:
-                print "In first if, fields[1]",fields[:]
-                #self.TargetType=str(fields[1])
-                self.TargetID=fields[0]
-                self.TargetType=fields[1]
-                self.RA2000=float(fields[2])
-                self.DE2000=float(fields[3])
-                self.Distance=float(fields[4])
-                self.DistUnits=fields[5]
-                self.mV=float(fields[6])
-
-
-class ObsDataList:
+class ObsDataList(CF.readtextfilelines):
     """
     This class is the container for a list of objects taken from an ASCII
     catalog. In this case, it is configured to read a list of HII regions 
-    that have been manually tagged in Aladin. It needs to be extended to 
+    that have been manually tagged in Aladin. It could be extended to 
     read from professional catalogs, either as exported by Aladin or directly
     from the Web.
     
     Another addition here could be photometric data for HII regions. 
     
-    Finally, even this class has a lot in common with the galaxy meta-data
-    class above. Together they are candidates for consolidation into a master
-    class.
     
     SMH 1/9/18
+    SMH 2/21/19 - updated to use Utils library
     """
-    def __init__(self,CatalogListFile):
+    pass
+    def loadcatalog(self):
 #Object,Cont_Flag,RAJ2000,DEJ2000,X,Y,Label_Flag,Info        
+        FirstTime=True
+        self.ObjectIdentifierDD=[]
+        self.Cont_Flag=[]
+        self.RAJ2000=[]
+        self.DEJ2000=[]
+        self.X=[]
+        self.Y=[]
+        self.Label_Flag=[]
+        self.Info=[]
+        self.NObs=0
         
-        #The initial plan is to read ALL records in the observation list
-        self.ObjectIdentifierDD=['']  #Keyword for source identification
-        self.Cont_Flag=['']           #Unknowns
-        self.RAJ2000=[0.0]            #RA of source in degrees
-        self.DEJ2000=[0.0]            #DE of source in degrees
-        self.X=[0.0]                  #Unknown
-        self.Y=[0.0]                  #Unknown
-        self.Label_Flag=['']          #Unknown
-        self.Info=['']                #Free text comment/info
-        self.NObs=0                   #Number of sources
-        
-        self.FirstTime=True
-        
-        CfgFile=open(CatalogListFile,'r')
-        CfgLines=CfgFile.readlines()
-        CfgFile.close()
-        nrecords=len(CfgLines)
-        #print CfgLines,nrecords
-
-        for recordindex in range(1,nrecords):
-            fields=CfgLines[recordindex].split(',')
-            #print fields
-            if self.FirstTime:
-                self.ObjectIdentifierDD[0]=str(fields[0])
-                self.Cont_Flag[0]=str(fields[1])
-                self.RAJ2000[0]=float(fields[2])
-                self.DEJ2000[0]=float(fields[3])
-                self.X[0]=float(fields[4])
-                self.Y[0]=float(fields[5])
-                self.Label_Flag[0]=str(fields[6])
-                self.Info[0]=str(fields[7])
-                self.FirstTime=False
-                self.NObs=1
-            else:
-                self.ObjectIdentifierDD.extend([str(fields[0])])
-                self.Cont_Flag.extend([str(fields[1])])
-                self.RAJ2000.extend([float(fields[2])])
-                self.DEJ2000.extend([float(fields[3])])
-                self.X.extend([float(fields[4])])
-                self.Y.extend([float(fields[5])])
-                self.Label_Flag.extend([str(fields[6])])
-                self.Info.extend([str(fields[7])])
-                self.NObs=self.NObs+1
+        for recordindex in range(1,self.nrecords):
+            fields=self.CfgLines[recordindex].split(',')
+            self.ObjectIdentifierDD.extend([str(fields[0])])
+            self.Cont_Flag.extend([str(fields[1])])
+            self.RAJ2000.extend([float(fields[2])])
+            self.DEJ2000.extend([float(fields[3])])
+            self.X.extend([float(fields[4])])
+            self.Y.extend([float(fields[5])])
+            self.Label_Flag.extend([str(fields[6])])
+            self.Info.extend([str(fields[7])])
+            self.NObs=self.NObs+1
+        print self.NObs
                 
 def PlotHII(Target,X_data,Y_data,plotparams):                
 #Plot Layout Configuration
---
+++ Galaxy_HII_Analysis.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 09 06:42:53 2017
+
+This function produces plots of HII distributions in galaxies. It uses 
+standardized classes to read metadata, control data, and observation data.
+It produces a single chart with three component plots.
+    1) A histogram of HII regions radial bins from the galaxy center
+    2) A plot of HII region density as a function of radial distance
+    3) The two-dimensional distribution of HII regions
+Current limitations include: only plotting data for a single galaxy at a time
+and being limited to plane-of-sky (POS) coordinates (as opposed to galaxy
+coordinates)
+
+
+@author: Astronomy
+"""
+
+def Galaxy_HII_Regions(drive,Target,TargetType):
+    import pylab as pl
+    import numpy as np
+    import GalaxyLIB as GX
+    import ConfigFiles as CF
+    import PlotUtils as PU
+    
+    #Valid for M33, M81, and M101
+    #Example call:
+    #Galaxy_HII_Regions("f:","M101","Galaxies")
+    
+    path=drive+"/Astronomy/Python Play/Galaxies/"
+    
+    
+    #Parameter Initialization
+    HISTparams=PU.PlotSetup(path+"HIIPlotConfig.txt")
+    HISTparams.loadplotparams(drive,Target,"POSHist")
+    DENSparams=PU.PlotSetup(path+"HIIPlotConfig.txt")
+    DENSparams.loadplotparams(drive,Target,"POSDens")
+    LOCparams=PU.PlotSetup(path+"HIIPlotConfig.txt")
+    LOCparams.loadplotparams(drive,Target,"POSLoc")
+    
+    GXparams=CF.Target_Parameters(path+"Galaxy_Parameters.txt")
+    GXparams.loadtargetparams(Target)
+    
+    #Reading observational data  and transforming the origin to the galactic center
+    path=drive+"/Astronomy/Projects/Galaxies/"+Target+"/Imaging Data/Mapping/"
+    fn=HISTparams.DataFile
+    DataList=GX.ObsDataList(path+fn)
+    DataList.loadcatalog()
+    print DataList.RAJ2000
+    
+    dr=(np.array(DataList.RAJ2000)-GXparams.RA2000)*60. #in arc minutes
+    dd=(np.array(DataList.DEJ2000)-GXparams.DE2000)*60 #in arc minutes
+    radii=np.sqrt(dr**2.+dd**2.)
+    #Computing the distribution histogram and density as a function of radius
+    nb=np.arange(HISTparams.X0,HISTparams.X1+1,HISTparams.DX)
+    hist=np.histogram(radii, bins=nb)
+    bin_centers=np.arange(1,HISTparams.X1+1,HISTparams.DX)
+    bin_areas=np.pi*((bin_centers+1)**2-(bin_centers-1)**2)
+    density=hist[0]/bin_areas
+    #Set up the canvas for plotting
+    pl.figure(figsize=(10., 4.), dpi=150, facecolor="white")
+    ###############################################################################
+    ax = pl.subplot2grid((2, 5), (0, 0))
+    ax0 = pl.subplot2grid((2, 5), (0, 0), colspan=3)
+    #pl.subplot(2, 1, 1)
+    successC=GX.PlotHII(Target,radii,nb,HISTparams) 
+    ###############################################################################
+    #pl.subplot(2, 1, 2)
+    ax1 = pl.subplot2grid((2, 5), (1, 0), colspan=3)
+    successC=GX.PlotHII(Target,bin_centers,density,DENSparams) 
+    
+    ###############################################################################
+    ax2 = pl.subplot2grid((2, 5), (0, 3), colspan=2, rowspan=2)
+    successC=GX.PlotHII(Target,dr,dd,LOCparams) 
+    ###############################################################################
+    pl.subplots_adjust(left=0.06, bottom=0.09, right=0.98, top=0.95,
+                wspace=0.4, hspace=0.3)
+    pl.savefig(path+Target+" H II Region Distribution.png",dpi=300)
